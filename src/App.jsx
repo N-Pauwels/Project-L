f80915cd496d0badc85ba38dd59200c004980366
NPAUWELS8 <144807305+NPAUWELS8@users.noreply.github.com>
2025-07-07 21:38:00 +0200

    Hero section completed for now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Hero from './sections/Hero.jsx'
+
 const App = () => {
   return (
-    <main>
-        <div>
-            <h1 className="text-5xl">Welcome!</h1>
-        </div>
-    </main>
+    <>
+      <Hero/>
+    </>
   )
 }
 
